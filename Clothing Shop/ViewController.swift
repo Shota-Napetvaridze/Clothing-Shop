052bb0dfd69153240fddfd5ab7502caa5c3c9b97
koshokosho <napetvaridze.shota@gmail.com>
2021-05-21 13:51:18 +0400

    Initial Commit

---
+++ Clothing Shop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clothing Shop
+//
+//  Created by MacBook Pro on 21.05.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
